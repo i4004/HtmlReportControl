e9283cc1c8b0e37bfc71764e504d2a28ad55e12c
Alexanderius <alexander.krylkov@gmail.com>
2013-10-25 23:13:15 +0600

    ExcelRichTextHtmlUtility  functionality

---
+++ HtmlReportControl/HtmlReport.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HtmlReportControl
+{
+	partial class HtmlReport
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._webBrowser = new System.Windows.Forms.WebBrowser();
+			this._buttonPrint = new System.Windows.Forms.Button();
+			this._buttonExport = new System.Windows.Forms.Button();
+			this._buttonSearch = new System.Windows.Forms.Button();
+			this._textBoxSearchText = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this._saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// _webBrowser
+			// 
+			this._webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._webBrowser.Location = new System.Drawing.Point(0, 43);
+			this._webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+			this._webBrowser.Name = "_webBrowser";
+			this._webBrowser.Size = new System.Drawing.Size(940, 567);
+			this._webBrowser.TabIndex = 0;
+			// 
+			// _buttonPrint
+			// 
+			this._buttonPrint.Location = new System.Drawing.Point(87, 6);
+			this._buttonPrint.Name = "_buttonPrint";
+			this._buttonPrint.Size = new System.Drawing.Size(75, 23);
+			this._buttonPrint.TabIndex = 6;
+			this._buttonPrint.Text = "Печать...";
+			this._buttonPrint.UseVisualStyleBackColor = true;
+			this._buttonPrint.Click += new System.EventHandler(this.OnButtonPrintClick);
+			// 
+			// _buttonExport
+			// 
+			this._buttonExport.Location = new System.Drawing.Point(6, 6);
+			this._buttonExport.Name = "_buttonExport";
+			this._buttonExport.Size = new System.Drawing.Size(75, 23);
+			this._buttonExport.TabIndex = 7;
+			this._buttonExport.Text = "Экспорт...";
+			this._buttonExport.UseVisualStyleBackColor = true;
+			this._buttonExport.Click += new System.EventHandler(this.OnButtonExportClick);
+			// 
+			// _buttonSearch
+			// 
+			this._buttonSearch.Location = new System.Drawing.Point(369, 6);
+			this._buttonSearch.Name = "_buttonSearch";
+			this._buttonSearch.Size = new System.Drawing.Size(75, 23);
+			this._buttonSearch.TabIndex = 8;
+			this._buttonSearch.Text = "Поиск";
+			this._buttonSearch.UseVisualStyleBackColor = true;
+			this._buttonSearch.Click += new System.EventHandler(this.OnButtonSearchClick);
+			// 
+			// _textBoxSearchText
+			// 
+			this._textBoxSearchText.Location = new System.Drawing.Point(168, 8);
+			this._textBoxSearchText.Name = "_textBoxSearchText";
+			this._textBoxSearchText.Size = new System.Drawing.Size(195, 20);
+			this._textBoxSearchText.TabIndex = 9;
+			this._textBoxSearchText.TextChanged += new System.EventHandler(this.OnSearchTextChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this._buttonSearch);
+			this.panel1.Controls.Add(this._buttonExport);
+			this.panel1.Controls.Add(this._textBoxSearchText);
+			this.panel1.Controls.Add(this._buttonPrint);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Padding = new System.Windows.Forms.Padding(3);
+			this.panel1.Size = new System.Drawing.Size(940, 37);
+			this.panel1.TabIndex = 10;
+			// 
+			// _saveFileDialog
+			// 
+			this._saveFileDialog.DefaultExt = "html";
+			this._saveFileDialog.Filter = "HTML files|*.html";
+			// 
+			// HtmlReport
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this._webBrowser);
+			this.Margin = new System.Windows.Forms.Padding(0);
+			this.Name = "HtmlReport";
+			this.Size = new System.Drawing.Size(940, 610);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser _webBrowser;
+		private System.Windows.Forms.Button _buttonPrint;
+		private System.Windows.Forms.Button _buttonExport;
+		private System.Windows.Forms.Button _buttonSearch;
+		private System.Windows.Forms.TextBox _textBoxSearchText;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.SaveFileDialog _saveFileDialog;
+	}
+}
---
+++ HtmlReportControl/HtmlReport.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+using mshtml;
+
+namespace HtmlReportControl
+{
+	public partial class HtmlReport : UserControl
+	{
+		public HtmlReport()
+		{
+			InitializeComponent();
+
+			ValidateSearchButtons();
+			_webBrowser.Navigate("about:blank");
+		}
+
+		/// <summary>
+		/// Load report to the browser control
+		/// </summary>
+		/// <param name="htmlData">Report html text</param>
+		/// <returns></returns>
+		public bool LoadReport(string htmlData)
+		{
+			if(_webBrowser.Document == null)
+				return false;
+
+			_webBrowser.Document.Write(htmlData);
+
+			return true;
+		}
+		
+		private void OnButtonExportClick(object sender, EventArgs e)
+		{
+			if(_saveFileDialog.ShowDialog() == DialogResult.OK)
+				File.WriteAllText(_saveFileDialog.FileName, _webBrowser.DocumentText);
+		}
+
+		private void OnButtonPrintClick(object sender, EventArgs e)
+		{
+			//const string keyName = @"Software\Microsoft\Internet Explorer\PageSetup";
+			//using(var key = Registry.CurrentUser.OpenSubKey(keyName, true))
+			//{
+			//	if(key == null)
+			//		return;
+
+			//	var oldFooter = (string)key.GetValue("footer");
+			//	var oldHeader = (string)key.GetValue("header");
+
+			//	try
+			//	{
+			//		key.SetValue("footer", "");
+			//		key.SetValue("header", "");
+
+			_webBrowser.ShowPrintPreviewDialog();
+			//_webBrowser.Pr
+			//	}
+			//	finally
+			//	{
+			//		key.SetValue("footer", oldFooter);
+			//		key.SetValue("header", oldHeader);
+			//	}
+			//}
+		}
+		
+		private void OnButtonSearchClick(object sender, EventArgs e)
+		{
+			Search();
+		}
+
+		private void Search()
+		{
+			if(_webBrowser.Document == null)
+				return;
+
+			var htmlDocument = _webBrowser.Document.DomDocument as IHTMLDocument2;
+
+			if(htmlDocument == null)
+				return;
+
+			var textRange = (IHTMLTxtRange)htmlDocument.selection.createRange();
+
+			textRange.collapse(false);
+			textRange.findText(_textBoxSearchText.Text, 1); // positive value, should search forward, negative backward
+			textRange.@select();
+
+			//if(result == false)
+			//ProgramHelper.ShowMessageBox()
+		}
+
+		private void OnSearchTextChanged(object sender, EventArgs e)
+		{
+			ValidateSearchButtons();
+		}
+
+		private void ValidateSearchButtons()
+		{
+			_buttonSearch.Enabled = _textBoxSearchText.Text.Length > 0;
+		}
+
+		//private void OnExportToMsExcel(object sender, EventArgs e)
+		//{
+			//if(File.Exists("test.xlsx"))
+			//	File.Delete("test.xlsx");
+
+			//// templateName = the name of .xlsx file
+			//using(var result = new FileStream("test.xlsx", FileMode.CreateNew))
+			//{
+			//	//using (var source = System.IO.File.OpenRead(""))
+			//	using(var excel = new OfficeOpenXml.ExcelPackage(result))
+			//	{
+
+			//		// Fill cells here
+			//		// Leave headers etc as is
+			//		excel.Workbook.Worksheets.Add("April 2012");
+			//		ExcelWorksheet ws = excel.Workbook.Worksheets[1]; // 1 is the position of the worksheet
+			//		ws.Name = "April 2012";
+
+			//		int rowIndex = 1;
+			//		int colIndex = 1;
+
+			//		do
+			//		{
+			//			// Set the background colours
+			//			var cell = ws.Cells[rowIndex, colIndex];
+			//			var fill = cell.Style.Fill;
+			//			fill.PatternType = ExcelFillStyle.Solid;
+			//			fill.BackgroundColor.SetColor(Color.LightGray);
+			//			colIndex++;
+			//		}
+			//		while(colIndex != 4);
+
+			//		// Set the cell values
+			//		var cell_actionName = ws.Cells[1, 1];
+			//		var cell_timeTaken = ws.Cells[1, 2];
+			//		var cell_processorsUsed = ws.Cells[1, 3];
+
+			//		cell_actionName.Value = "Action Name";
+			//		cell_timeTaken.Value = "Time Taken";
+			//		cell_processorsUsed.Value = "Processing Unit";
+
+			//		excel.Save();
+			//	}
+			//}
+		//}
+
+		//private void OnExportToPdf(object sender, EventArgs e)
+		//{
+			//iTextSharp.text.DocWriter
+			//using (Stream stream = new FileStream("test.pdf", FileMode.CreateNew))
+			//{
+			//	var pdfWorker = new HtmlToPdfConverter();
+			//	pdfWorker.Open(stream);
+			//	pdfWorker.Run("test");
+			//	pdfWorker.Close();
+			//}
+
+			//createPDF(_webBrowser.DocumentText);
+			//a.S
+		//}
+
+		//private void createPDF(string html)
+		//{
+		//	if (File.Exists("test.pdf"))
+		//		File.Delete("test.pdf");
+
+		//	using (Stream stream = new FileStream("test.pdf", FileMode.CreateNew))
+		//	{
+		//		//MemoryStream msOutput = new MemoryStream();
+		//		TextReader reader = new StringReader(html);
+
+		//		// step 1: creation of a document-object
+		//		Document document = new Document(PageSize.A4, 30, 30, 30, 30);
+
+		//		// step 2:
+		//		// we create a writer that listens to the document
+		//		// and directs a XML-stream to a file
+		//		PdfWriter writer = PdfWriter.GetInstance(document, stream);
+
+		//		// step 3: we create a worker parse the document
+		//		HTMLWorker worker = new HTMLWorker(document);
+
+		//		// step 4: we open document and start the worker on the document
+		//		document.Open();
+		//		worker.StartDocument();
+
+		//		// step 5: parse the html into the document
+		//		worker.Parse(reader);
+
+		//		// step 6: close the document and the worker
+		//		worker.EndDocument();
+		//		worker.Close();
+		//		document.Close();
+		//	}
+
+		//	//return msOutput;
+		//}
+	}
+}
